069db6ed9828fb958fd35adb20f144af75d64b9d
Sophiys <sophiys@mail.ru>
2023-04-07 03:49:57 +0300

    result

--- Color Sliders/ViewController.swift
+++ Color Sliders/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorChangingView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        redSliderAction()
+        greenSliderAction()
+        blueSliderAction()
     }
-
-
+    
+    private func changeColor() {
+        colorChangingView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+    
+    @IBAction func redSliderAction() {
+        redLabel.text = "Red:\n\((round(redSlider.value*100)/100).formatted())"
+        changeColor()
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenLabel.text = "Green:\n\((round(greenSlider.value*100)/100).formatted())"
+        changeColor()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueLabel.text = "Blue:\n\((round(blueSlider.value*100)/100).formatted())"
+        changeColor()
+    }
+    
+    
 }
 
